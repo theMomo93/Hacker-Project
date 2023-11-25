d3eeb0599348a63d51aa28a9804ea76125108978
theMomo93 <mkaminska93@gmail.com>
2023-11-25 10:25:29 +0100

    Boilerplate react-Dom

---
+++ src/Router.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+function Router() {
+ 
+  return (
+    <BrowserRouter>
+      <Routes>
+
+
+      </Routes>
+    </BrowserRouter>
+  );
+
+
+
+
+
+}
+
+export default Router
--- src/components/Greeting.jsx
+++
@@ -1,9 +0,0 @@
-function Greeting(){
-    return (
-        <div className="greeting">
-        Hello, React beginner!   
-        </div>
-    );
-};
-
-export default Greeting    /* Important*/
