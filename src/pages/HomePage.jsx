af9c73b43a7df59a74c53d02ebe2636df455034a
theMomo93 <mkaminska93@gmail.com>
2023-11-25 10:55:01 +0100

    fetched data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,31 +1,27 @@
-import React from 'react'
-import { useState, useEffect } from "react";
-
-
-
+import React, { useState, useEffect } from 'react';
 
 export function HomePage() {
-    const [news, setNews]=useState({});
+  const [news, setNews] = useState([]);
 
-    useEffect(()=>{
-        async function getData(){
-            try{
-                const response = await fetch("https://hn.algolia.com/api");
-                const data = await response.json();
-                setNews(data);
-                
-            }catch(error){
-                console.log("Error fetching random dog image:", error);
-            }
-          }
-            getData();
-        },{})
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await fetch('https://hn.algolia.com/api/v1/search?query=react');
+        const data = await response.json();
+        setNews(data.hits);
+      } catch (error) {
+        console.log('Error fetching data:', error);
+      }
+    }
+
+    getData();
+  }, []);
 
   return (
-    <div>HomePage
-        <ul>
-            <li></li>
-        </ul>
+    <div>
+      {news.map((item) => (
+        <div key={item.objectID}>{item.title}</div>
+      ))}
     </div>
-  )
+  );
 }
