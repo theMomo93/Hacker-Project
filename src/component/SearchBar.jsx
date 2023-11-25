615dcaf90036e30a12b48a2d220990bee47bd2f9
Kogha97 <poiuytrewq7991@gmail.com>
2023-11-25 11:14:37 +0000

    search bar

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
 
 export function SearchBar() {
+  const [search, setSearch] = useState({});
+
+  useEffect(() =>{
+    fetch('')
+    .then((response) => response.json())
+    .then((data) =>setSearch(data))
+    .catch((error) => console.log("Error fetching Data: ", error))
+  })
   return (
     <div>
         <p>Search Bar</p>
