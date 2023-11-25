3837f3e53141f8b7fce8b561f194fc7f1f92e92f
Kogha97 <poiuytrewq7991@gmail.com>
2023-11-25 11:44:40 +0000

    fecthing data for stories

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -1,22 +1,51 @@
-import React from 'react'
-import {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 
 export function SearchBar() {
-  const [search, setSearch] = useState({});
+  const [searchInput, setSearchInput] = useState('');
+  const [selectedTag, setSelectedTag] = useState('story');
+  const [searchResults, setSearchResults] = useState({});
 
-  useEffect(() =>{
-    fetch('')
-    .then((response) => response.json())
-    .then((data) =>setSearch(data))
-    .catch((error) => console.log("Error fetching Data: ", error))
-  })
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `https://hn.algolia.com/api/v1/search?query=${searchInput}&tags=${selectedTag}`
+      );
+      const data = await response.json();
+      setSearchResults(data);
+    } catch (error) {
+      console.log('Error fetching data: ', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [searchInput, selectedTag]);
+
   return (
     <div>
-        <p>Search Bar</p>
-        <input type="text" />
-        <button>Search</button>
-
-
+      <p>Search Bar</p>
+      <input
+        type="text"
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+      />
+      <button onClick={() => fetchData()}>Search</button>
+      <select
+        name="tags"
+        value={selectedTag}
+        onChange={(e) => setSelectedTag(e.target.value)}
+      >
+        <option value="story">Story</option>
+        <option value="comment">Comment</option>
+        <option value="user">User</option>
+      </select>
+      {searchResults.hits && (
+        <ul>
+          {searchResults.hits.map((hit) => (
+            <li key={hit.objectID}>{hit.title}</li>
+          ))}
+        </ul>
+      )}
     </div>
-  )
+  );
 }
