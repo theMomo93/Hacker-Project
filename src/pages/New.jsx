6437868c4674f3cf94cb60024c0893d53efa0be8
theMomo93 <mkaminska93@gmail.com>
2023-11-25 13:47:33 +0100

    new page added

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 
 
-export function HomePage() {
+export function New() {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
     async function getData() {
       try {
-        const response = await fetch('https://hn.algolia.com/api/v1/search?tags=front_page');
+        const response = await fetch('http://hn.algolia.com/api/v1/search_by_date?tags=story');
         const data = await response.json();
         setNews(data.hits);
       } catch (error) {
@@ -40,7 +40,7 @@ export function HomePage() {
         <p>|</p>
         <p>Hide </p>
         <p>|</p>
-        <p key={item.id}> {item.num_comments}Comments</p>      
+        <p key={item.id}> {item.num_comments} comments </p>      
         <p>|</p>
         </div>
         </li>
