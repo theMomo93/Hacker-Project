8cdb2a586e51287db6a5a829957747611c90b89c
theMomo93 <mkaminska93@gmail.com>
2023-11-28 11:17:45 +0100

    typo, hours work

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -14,27 +14,27 @@ export default function SearchResults() {
             <ol className='list-container'>
               {searchResults.hits.map((hit) => {
 
-                const createdAtDate = new Date(hit.create_at);
+                const createdAtDate = new Date(hit.created_at);
                 const currentDate = new Date();
                 const timeDifference = currentDate - createdAtDate;
                 const hoursDifference = timeDifference / (1000*60*60)
                 console.log(hoursDifference)
                 return (
-                  <li key={hit.objectID}>
-                    {hit.title}
-                    <div className='list-items'>
-                      <p key={hit.objectID}>{hit.points} points </p>
-                      <p>|</p>
-                      <p key={hit.objectID}> {hit.author}</p>
-                      <p>|</p>
-                      <p key={hit.objectID}>{hoursDifference.toFixed(0)}</p>
-                      <p>|</p>
-                      <p>Hide </p>
-                      <p>|</p>
-                      <p key={hit.objectID}> {hit.num_comments} Comments</p>
-                      <p>|</p>
-                    </div>
-                  </li>
+                  <li key={hit.id}>
+                 {hit.title}
+                  <div className='list-items'>
+                   <p>{hit.points} points </p>
+                  <p>|</p>
+                 <p>{hit.author}</p>
+                 <p>|</p>
+                 <p>{hoursDifference.toFixed(0)} hours ago</p>
+                 <p>|</p>
+                 <p>Hide </p>
+                 <p>|</p>
+                  <p>{hit.num_comments} Comments</p>
+                  <p>|</p>
+                </div>
+              </li>
                 );
               })}
             </ol>
