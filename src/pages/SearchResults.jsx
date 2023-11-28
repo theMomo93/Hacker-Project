358825d8aa522ab0a9fd5daf22c9b773f0ef8823
Kogha97 <poiuytrewq7991@gmail.com>
2023-11-28 09:33:04 +0000

    search bar display working

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -6,16 +6,35 @@ import { SearchBar } from '../component/SearchBar';
 export default function SearchResults() {
     const { state } = useLocation();
     const searchResults = state && state.searchResults
+
+
   return (
     <div>
-        <h2>Search Results</h2>
+    <h2 className='title-container'>Search Results</h2>
+    <div className='article-container'>
+   
         {searchResults && searchResults.hits && (
-            <ul>
+            <ol className='list-container'>
                 {searchResults.hits.map((hit) => (
-                    <li key={hit.objectID}>{hit.title}</li>
+                    <li key={hit.objectID}>
+                    {hit.title}
+                        <div className='list-items'>
+                        <p key={hit.objectIDid}>{hit.points} points </p>
+                        <p>|</p>
+                        <p key={hit.objectIDid}> {hit.author}</p>
+                        <p>|</p>
+                        <p key={hit.objectIDid}>{} hours ago</p>
+                        <p>|</p>
+                        <p>Hide </p>
+                        <p>|</p>
+                        <p key={hit.objectIDid}> {hit.num_comments} Comments</p>
+                        <p>|</p>
+                        </div>
+                    </li>
                 ))}
-            </ul>
+            </ol>
         )}
+    </div>
     </div>
   )
 }
