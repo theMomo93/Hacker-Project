0463aef5cfd16b2ca42c284902d090b2094bccdb
theMomo93 <mkaminska93@gmail.com>
2023-12-02 11:07:45 +0100

    search updated

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,73 +1,84 @@
-import React from 'react'
+import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { SearchBar } from '../component/SearchBar';
 
 export default function SearchResults() {
-    const { state } = useLocation();
-    const searchResults = state && state.searchResults;
-  
-    return (
-      <div>
-        <h2 style={{color:'green', fontSize: "50px", textDecoration:"underline"}}className='title-container'>-Search Results for: {searchResults?.query}-</h2>
-        <div className='article-container'>
-          {searchResults && searchResults.hits && (
-            <ol className='list-container'>
-              {searchResults.hits.map((hit) => {
+  const { state } = useLocation();
+  const searchResults = state && state.searchResults;
 
-                const createdAtDate = new Date(hit.created_at);
-                const currentDate = new Date();
-                const timeDifference = currentDate - createdAtDate;
-                const hoursDifference = timeDifference / (1000*60*60)
-                let currentTime
-                let dating
-                if( hoursDifference < 1 ){
-                    currentTime = hoursDifference
-                    dating = "Hour"
-                }
-                else if( hoursDifference <= 24 && hoursDifference > 1){
-                    currentTime = hoursDifference
-                    dating = "Hours"
-                }
-                else if (hoursDifference < 168){
-                    currentTime = hoursDifference / 24
-                    dating = "Days"
-                }
-                else if(hoursDifference >= 168  && hoursDifference < 730 ){
-                   currentTime = hoursDifference / 168 // this defines weeks
-                    dating = "Weeks"
-                } else if ( hoursDifference > 730 && hoursDifference < 8760){
-                    currentTime = hoursDifference / 730 // this defines months
-                    dating = "Months"
-                }
-                else if (hoursDifference >= 8760 ){
-                    currentTime = hoursDifference / 8760;
-                    dating = "Years"
-                }
-             
-                return (
-                  <li className="titleSearch" key={hit.id}>
-                 <a className='search-title' href={hit._highlightResult?.url?.value || '#'} target='_blank' rel='noopener noreferrer'>
-                 {hit.title}
-                 </a>
+  // Function to highlight occurrences of the search query in the title
+  const highlightSearchQuery = (title) => {
+    if (!searchResults || !searchResults.query) {
+      return title;
+    }
+
+    const query = searchResults.query.toLowerCase();
+    const regex = new RegExp(`(${query})`, 'gi');
+
+    return title.split(regex).map((part, index) => (
+      regex.test(part) ? <span key={index} style={{ backgroundColor: 'green' }}>{part}</span> : part
+    ));
+  };
+
+  return (
+    <div>
+      <h2 style={{ color: 'green', fontSize: '50px', textDecoration: 'underline' }}>
+        -Search Results for: {searchResults?.query}-
+      </h2>
+      <div className='article-container'>
+        {searchResults && searchResults.hits && (
+          <ol className='list-container'>
+            {searchResults.hits.map((hit) => {
+              const createdAtDate = new Date(hit.created_at);
+              const currentDate = new Date();
+              const timeDifference = currentDate - createdAtDate;
+              const hoursDifference = timeDifference / (1000 * 60 * 60);
+              let currentTime;
+              let dating;
+
+              if (hoursDifference < 1) {
+                currentTime = hoursDifference;
+                dating = 'Hour';
+              } else if (hoursDifference <= 24 && hoursDifference > 1) {
+                currentTime = hoursDifference;
+                dating = 'Hours';
+              } else if (hoursDifference < 168) {
+                currentTime = hoursDifference / 24;
+                dating = 'Days';
+              } else if (hoursDifference >= 168 && hoursDifference < 730) {
+                currentTime = hoursDifference / 168; // this defines weeks
+                dating = 'Weeks';
+              } else if (hoursDifference > 730 && hoursDifference < 8760) {
+                currentTime = hoursDifference / 730; // this defines months
+                dating = 'Months';
+              } else if (hoursDifference >= 8760) {
+                currentTime = hoursDifference / 8760;
+                dating = 'Years';
+              }
+
+              return (
+                <li className="titleSearch" key={hit.id}>
+                  <a className='search-title' href={hit._highlightResult?.url?.value || '#'} target='_blank' rel='noopener noreferrer'>
+                    {highlightSearchQuery(hit.title)}
+                  </a>
                   <div className='list-items'>
-                   <p>{hit.points} points </p>
-                  <p>|</p>
-                 <p>{hit.author}</p>
-                 <p>|</p>
-                 <p>{currentTime.toFixed(0)} {dating} ago</p>
-                 <p>|</p>
-                 <p>Hide </p>
-                 <p>|</p>
-                  <p>{hit.num_comments} Comments</p>
-                  <p>|</p>
-                </div>
-              </li>
-                );
-              })}
-            </ol>
-          )}
-        </div>
+                    <p>{hit.points} points </p>
+                    <p>|</p>
+                    <p>{hit.author}</p>
+                    <p>|</p>
+                    <p>{currentTime.toFixed(0)} {dating} ago</p>
+                    <p>|</p>
+                    <p>Hide </p>
+                    <p>|</p>
+                    <p>{hit.num_comments} Comments</p>
+                    <p>|</p>
+                  </div>
+                </li>
+              );
+            })}
+          </ol>
+        )}
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
